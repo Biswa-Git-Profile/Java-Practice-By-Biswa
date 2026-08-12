2e39e7f6c71c19f7896b247a98618b1d9576aec2
Kanha <biswaranjandas7108@gmail.com>
2026-08-12 13:11:00 +0530

    Added even and odd number programs

---
+++ src/Basics/PrintEvenNumber.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class PrintEvenNumber {
+   public static void main(String[] args) {
+        for (int i = 1; i <= 90; i++) {
+            if (i % 2 == 0) {
+            System.out.println(i);
+        }
+    }
+}}
