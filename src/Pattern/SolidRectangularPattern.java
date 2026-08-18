7cf7717bc13703e47e7d33bf3724fc971b21b4a3
Kanha <biswaranjandas7108@gmail.com>
2026-08-18 11:36:47 +0530

    Added This SolidRectangular Pattern Program

---
+++ src/Pattern/SolidRectangularPattern.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class SolidRectangularPattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
