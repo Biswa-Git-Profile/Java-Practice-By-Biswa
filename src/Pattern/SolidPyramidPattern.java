8f9ec2fb47055e038336c30b644579ce542a6df3
Kanha <biswaranjandas7108@gmail.com>
2026-08-19 09:05:57 +0530

    Added This InSolidPyramidPattern Pattern Program

---
+++ src/Pattern/SolidPyramidPattern.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class SolidPyramidPattern {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++) {
+            for (int k = 1; k <= 5 - i ; k++) {
+                System.out.print(" ");}
+
+                for (int j = 1; j <= 2*i - 1; j++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+        }
+    }
