784223fbaa905b9fa84967d1991c0b686b236069
Kanha <biswaranjandas7108@gmail.com>
2026-08-24 10:45:44 +0530

    Added This Simplemethodprogram with statickeyword Pattern program

---
+++ src/Methods/Simplemethodprogram.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Simplemethodprogram {
+
+        static void print2table(){
+            for (int i = 1; i <=10 ; i++) {
+                int ans=2*i;
+                System.out.println("-> "+ans);
+            }
+
+        }
+        public static void main(String[] args) {
+            print2table();
+        }
+}
